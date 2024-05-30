72a9ea380833d28545de922d42a19df210704b4a
Dario <derioreal63@gmail.com>
2024-05-30 20:58:30 +0200

    fix css

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,7 +19,7 @@ export default function App() {
 
 
     return <>
-        <div>
+        <div className={"markdown"}>
             <div dangerouslySetInnerHTML={{__html: md}}/>
         </div>
     </>
