12e205dce67b1e4111393045b53a0a0cf3e3bdfb
Dario <derioreal63@gmail.com>
2024-06-02 15:28:23 +0200

    fix keys

---
+++ src/pages/pgp/index.tsx
@@ -0,0 +1,12 @@
+// @ts-nocheck
+"use client"
+
+
+export default function App() {
+
+    this.context.router.push("https://raw.githubusercontent.com/Knerio/knerio.github.io/main/public/public.asc")
+
+    return <>
+        REDIRECTING
+    </>
+}
---
+++ src/pages/ssh/index.tsx
@@ -0,0 +1,12 @@
+// @ts-nocheck
+"use client"
+
+
+export default function App() {
+
+    this.context.router.push("https://raw.githubusercontent.com/Knerio/knerio.github.io/main/public/id_rsa.pub")
+
+    return <>
+        REDIRECTING
+    </>
+}
