d3a1a207362df8d9a19f012fe725187e2609ef37
Dario Pranjic <96529060+Knerio@users.noreply.github.com>
2024-06-02 15:37:26 +0200

    Aktualisieren von index.tsx

--- src/pages/ssh/index.tsx
+++ src/pages/ssh/index.tsx
@@ -3,9 +3,11 @@
 
 
 export default function App() {
-
-    window.location.replace("https://raw.githubusercontent.com/Knerio/knerio.github.io/main/public/id_rsa.pub")
-
+ useEffect(function onFirstMount() {
+     window.location.replace("https://raw.githubusercontent.com/Knerio/knerio.github.io/main/public/id_rsa.pub")
+ 
+  }, [])
+   
     return <>
         REDIRECTING
     </>
