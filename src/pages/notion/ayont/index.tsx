5010bab6e4e1a02e9e4a86b99a1b3b2e2715feb2
Dario <derioreal63@gmail.com>
2024-06-04 22:30:34 +0200

    add notion/ayont page

---
+++ src/pages/notion/ayont/index.tsx
@@ -0,0 +1,15 @@
+"use client"
+
+import {useEffect} from "react";
+
+export default function App() {
+
+    useEffect(() => {
+        window.location.replace("https://knerio.notion.site/Ayont-Event-Live-Server-6cee53bc960f4e3793742a6a7f161955")
+    }, []);
+
+    return <>
+        <h1>Redirecting...</h1>
+    </>
+
+}
